0bd2c934bba230d5c53b9f30874a286f886be28a
Waldo1628 <41935443+Waldo1628@users.noreply.github.com>
2021-10-01 12:10:57 -0500

    Add files via upload

---
+++ Script.ps1
@@ -0,0 +1,7 @@
+﻿Connect-PnPOnline –Url https://myteams.toyota.com/sites/UTD-VendorCostStandardization/ –UseWebLogin
+
+$LibraryName = "Test"  
+$Files = Get-PnPFolderItem -FolderSiteRelativeUrl $LibraryName -ItemType File  
+foreach($File in $Files) {         
+    Get-PnPFile -Url $File.ServerRelativeUrl -Path D:\Downloads -FileName $File.Name -AsFile          
+    } 
