a04827be8a7a4820ca9e33f42d813df6bf547226
dartagnan009 <62113603+dartagnan009@users.noreply.github.com>
2021-10-22 16:06:48 -0500

    new db method using dataframe
    needs additional cleaning and more aspects to be improved upon

--- Database/parse.py
+++ Database/parse.py
@@ -3,26 +3,59 @@
 import re
 import sys
 import os
-
-#Searches string for words with 17 characters
-#Essentialy vin
-vin = re.compile(r'\w\w\w\w\w\w\w\w\w\w\w\w\w\w\d\d\d')
-
-with open ('test2.txt', 'r', encoding='utf-8') as f:
-    contents = f.read()
-
-    vin_matches = vin.finditer(contents)
-
-    for match in vin_matches:
-        #print(match)
-        vin_string = contents[match.start():match.end()]
-
-
+import pandas as pd
 
 
 class carObj:
 
-    def init(self, vin_num):
+    def __init__(self, vin_num):
         self.vin_num = vin_num
 
-p1 = carObj(vin_string)
+    def vin_total(self,vin_total):
+        self.vin_total = vin_total
+
+    def getList(self):
+        return '{}'.format(self.vin_num)
+
+#Searches string for words with 17 characters
+#Essentialy vin
+directory_filepath = '/Users/waldo/Desktop/db'
+vin = re.compile(r'\w\w\w\w\w\w\w\w\w\w\w\w\w\w\d\d\d')
+
+vin_list = []
+count = 0
+for files in os.listdir(directory_filepath):
+    if files.endswith(".txt"):
+        file = os.path.join(directory_filepath, files)
+        #print(file)
+
+        with open (file, 'r') as f:
+            contents = f.read()
+
+            vin_matches = vin.finditer(contents)
+
+            for match in vin_matches:
+                vin_string = contents[match.start():match.end()]
+                vin_list.append(vin_string)
+                count += len(vin_list)
+
+
+
+
+print(vin_list)
+
+# vin_table = {
+#     "vin": ['ABC123']
+# }
+
+# df = pd.DataFrame(vin_table)
+
+# print(df)
+
+# df = pd.DataFrame()
+
+df = pd.DataFrame(vin_list,columns=['Vin'])
+
+print(df)
+
+df.to_csv('dummy.csv')
