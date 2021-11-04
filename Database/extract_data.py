0dc097aba65c91ae3e8c7b7afea45d938061690e
dartagnan009 <62113603+dartagnan009@users.noreply.github.com>
2021-11-03 22:43:03 -0500

    fixed date and payment

--- Database/extract_data.py
+++ Database/extract_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 #Read in the directory passed in with the script
 #arg = sys.argv[1]
+#directory_filepath = arg
 directory_filepath = "C:\\Users\D'artagnan wong\Desktop\\toyota"
 
 #Create the patterns which we can use to extract fields
@@ -34,11 +35,11 @@ def find_service_type(file_contents):
     if mechanical_match or maintenance_match or labor_match:
         return 'Maintenance'
     elif plates_match:
-        return 'License Plate Renewel'
+        return 'License Plate Renewal'
     elif monthly_match or parking_match:
         return 'Parking' 
     else:
-        return 'Unknown'
+        return 'Unidentified'
 
 
 def find_payments(file_contents, vin_list):
@@ -90,48 +91,71 @@ for files in os.listdir(directory_filepath):
         else:
             print("Convert SUCCESS: ", file)
 
-        with open (file, 'r') as f:
+        with open (file, 'r', encoding='utf-8') as f:
             contents = f.read()
             
             extracted_vins = extract(vin_number, contents, 0, 0)
             
             potential_date = re.findall(r'(\d+/\d+/\d+)',contents)
+            #potential_date2 = re.compile(r'[A-Za-z]{3,9}\s[0-9]{1,2},\s\d\d\d\d', contents)
+            potential_date2 = re.findall(r'[A-Za-z]{3,9}\s[0-9]{1,2},\s\d\d\d\d', contents)
+           # print(potential_date2)
             date = "UNKNOWN"
             if len(potential_date)>0:
                 date = potential_date[0]
+            elif len(potential_date2) > 0:
+                date = potential_date2[0]
+                date_arr = date.split(', ')
+                month = date_arr[0].split(' ')[0]
+                day = date_arr[0].split(' ')[1]
+                year = date_arr[1]
+                date = month + '/' + day + '/' + year
+                #print(month)
+                #date = date_arr[1] + '/' + date_arr[0] + '/' +  date_arr[2]
+
             else:
                 file_name = files.split("_")
                 for parts in file_name:
                     if '.' in parts and len(parts) > 6:
                         date = parts
+                        date = date.replace('.', '/')
+                        date = date.replace('/txt', '')
                         break
             
             service_type = find_service_type(contents)
             
             payments_list = find_payments(contents,extracted_vins)
-            
-           # invoice_name = files.split("_")[0]
 
-            invoice_name = files
-           # filename = files
+
+            if service_type == 'Parking' and len(extracted_vins) >= 2 and len(payments_list) >= 1:
+                amount = payments_list[0]
+                clean_amount = amount[1:]
+                repeated_amount = float(clean_amount.replace(',',''))/len(extracted_vins)
+                for i in range(len(payments_list)):
+                    payments_list[i] = '$' + str(repeated_amount)
+               
+            
+            invoice_name = files.split("_")[0]
+
+            #invoice_name = files
+            filename = files
 
             dictionary = {}
             count = 0
+         
 
             for vin in extracted_vins:
-                if vin in dictionary:
-                    count+=1
-                else:
+                if vin not in dictionary:
                     dictionary[vin] = payments_list[count]
-                    #print(payments_list[count])
-                    count+=2
+                count+=1
+             
                 
 
      
             for keys in dictionary:
                 vin = keys
                 amount = dictionary[keys]
-                database.append( tuple((vin, amount, date, service_type, invoice_name)) )
+                database.append( tuple((vin, amount, date, service_type, invoice_name, filename)) )
      
         
         #print("VINs: ",extracted_vins)
@@ -139,6 +163,7 @@ for files in os.listdir(directory_filepath):
         print("Date: ", date)
         print("Type of service: ", service_type)
         print("Invoice Name: ", invoice_name)
+        print("Filename: ", filename)
         
         print("\n")
         #os.remove(file)
@@ -150,12 +175,14 @@ payment_records = []
 date_records = []
 service_type_records = []
 invoice_name_records = []
+filename_records = []
 for records in database:
     vin_records.append(records[0])
     payment_records.append(records[1])
     date_records.append(records[2])
     service_type_records.append(records[3])
     invoice_name_records.append(records[4])
+    filename_records.append(records[5])
 
     
 
@@ -164,6 +191,7 @@ df = df.assign(Payment=payment_records)
 df = df.assign(Date=date_records)
 df = df.assign(Service=service_type_records)
 df = df.assign(Invoice_Name=invoice_name_records)
+df = df.assign(Filename = filename_records)
 
 print(df)
 
